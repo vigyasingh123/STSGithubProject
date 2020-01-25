7a5dcaeb03950296124908539b70b104330b65eb
Vigya Singh <vigyasingh123@gmail.com>
2020-01-25 13:25:29 -0500

    First Commit

---
+++ STSGithubProject/src/main/java/com/vspractice/www/STSGithubProject/App.java
@@ -0,0 +1,13 @@
+package com.vspractice.www.STSGithubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
